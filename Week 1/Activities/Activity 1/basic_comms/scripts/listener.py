6ae4e90e64e038bdf50b1ae637fb242550d1f02d
Mario Martinez <mario.mtz@tec.mx>
2026-02-09 13:50:56 -0600

    Delete Week 1 directory

--- Week 1/Activities/Activity 1/basic_comms/scripts/listener.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
-
-def callback(msg):
-
-
-if __name__=='__main__':
--- Week 1/Activities/Activity 1/basic_comms/scripts/talker.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
-
-if __name__=='__main__':
-
-
-    while not rospy.is_shutdown():
